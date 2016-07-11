3733c39bb2ce766ae8ee852d34a52e06eadcf76d
jianghong-023 <jianghong_023@163.com>
2016-07-11 22:41:41 +0800

    add lseek function appliction

---
+++ unix-2th-3th/io/openat/openat.c
@@ -0,0 +1,17 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+
+int main(void)
+{
+    int fdat;
+
+    fdat = openat();
+    
+    exit(0);
+}
